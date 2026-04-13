542144011b59ab78bb7726ff71527a0a08bd2d9d
Ganasai-Palakurthi <ganasaip98@gmail.com>
2026-04-13 15:54:25 -0400

    added audit log

---
+++ sql/02-metadata/pipeline_run_log.sql
@@ -0,0 +1,18 @@
+USE DATABASE SALES_DWH_DEV;
+USE SCHEMA CONTROL;
+
+CREATE OR REPLACE TABLE PIPELINE_RUN_LOG (
+                                             RUN_ID NUMBER IDENTITY(1,1),
+                                             PIPELINE_NAME STRING,
+                                             SOURCE_TABLE STRING,
+                                             TARGET_TABLE STRING,
+                                             LOAD_TYPE STRING,
+                                             LOAD_MODE STRING,
+                                             STATUS STRING,
+                                             START_TIME TIMESTAMP_NTZ,
+                                             END_TIME TIMESTAMP_NTZ,
+                                             ROWS_LOADED NUMBER,
+                                             ERROR_MESSAGE STRING,
+                                             EXECUTED_SQL STRING,
+                                             CREATED_AT TIMESTAMP_NTZ DEFAULT CURRENT_TIMESTAMP()
+);
