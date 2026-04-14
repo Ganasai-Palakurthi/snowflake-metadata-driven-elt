0b81c0014e7884c0d862213511b6f21a0bb37a80
Ganasai-Palakurthi <ganasaip98@gmail.com>
2026-04-13 20:33:31 -0400

    product testing

--- sql/04_procedures/sp_load_pipeline.sql
+++ sql/04_procedures/sp_load_pipeline.sql
@@ -122,11 +122,11 @@ EXCEPTION
         UPDATE SALES_DWH_DEV.CONTROL.PIPELINE_RUN_LOG
         SET STATUS = 'FAILED',
             END_TIME = :V_END_TIME,
-            ERROR_MESSAGE = SQLERRM,
+            ERROR_MESSAGE = ERROR_MESSAGE(),
             EXECUTED_SQL = :V_SQL
         WHERE RUN_ID = :V_RUN_ID;
 
-        RETURN 'FAILED: Pipeline ' || PIPELINE_NAME_ARG || ' | Error: ' || SQLERRM;
-
+        RETURN 'FAILED: Pipeline ' || PIPELINE_NAME_ARG ||
+               ' | Error: ' || ERROR_MESSAGE();
 END;
 $$;
